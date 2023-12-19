4e38b36eed17daa6a65a3f959f2fa0376b745555
Chris Finlay <christopher.finlay@unige.ch>
2023-12-19 05:20:52 +0000

    Unet and init zero

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ class NoiseEmbedding1(nn.Module):
 
 
 class ResidualBlock1(nn.Module):
-    def __init__(self, nb_channels: int) -> None:
+    def __init__(self, nb_channels: int, init_zero: bool = False) -> None:
         super().__init__()
         self.norm1 = nn.BatchNorm2d(nb_channels)
         self.conv1 = nn.Conv2d(
@@ -50,6 +50,8 @@ class ResidualBlock1(nn.Module):
         self.conv2 = nn.Conv2d(
             nb_channels, nb_channels, kernel_size=3, stride=1, padding=1
         )
+        if init_zero:
+            torch.nn.init.zeros_(self.conv2.weight)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         y = self.conv1(F.relu(self.norm1(x)))
@@ -93,6 +95,7 @@ class Model22(nn.Module):
         nb_channels: int,
         num_blocks: int,
         cond_channels: int,
+        init_zero: bool = False,
     ) -> None:
         super().__init__()
         self.image_size = image_size
@@ -102,9 +105,11 @@ class Model22(nn.Module):
             image_channels + 1, nb_channels, kernel_size=3, padding=1
         )
         self.blocks = nn.ModuleList(
-            [ResidualBlock1(nb_channels) for _ in range(num_blocks)]
+            [ResidualBlock1(nb_channels, init_zero) for _ in range(num_blocks)]
         )
         self.conv_out = nn.Conv2d(nb_channels, image_channels, kernel_size=3, padding=1)
+        if init_zero:
+            torch.nn.init.zeros_(self.conv_out.weight)
 
     def forward(self, noisy_input: torch.Tensor, c_noise: torch.Tensor) -> torch.Tensor:
         cond = self.noise_fc1(self.noise_emb(c_noise))  # TODO: not used yet
@@ -130,15 +135,18 @@ class Model3(nn.Module):
         nb_channels: int,
         num_blocks: int,
         cond_channels: int,
+        init_zero: bool = False,
     ) -> None:
         super().__init__()
         self.noise_emb = NoiseEmbedding1(cond_channels)
         self.noise_fc1 = nn.Linear(cond_channels, 2 * nb_channels)
         self.conv_in = nn.Conv2d(image_channels, nb_channels, kernel_size=3, padding=1)
         self.blocks = nn.ModuleList(
-            [ResidualBlock2(nb_channels) for _ in range(num_blocks)]
+            [ResidualBlock2(nb_channels, init_zero) for _ in range(num_blocks)]
         )
         self.conv_out = nn.Conv2d(nb_channels, image_channels, kernel_size=3, padding=1)
+        if init_zero:
+            torch.nn.init.zeros_(self.conv_out.weight)
 
     def forward(self, noisy_input: torch.Tensor, c_noise: torch.Tensor) -> torch.Tensor:
         cond = self.noise_emb(c_noise)  # TODO: not used yet
@@ -149,7 +157,7 @@ class Model3(nn.Module):
 
 
 class ResidualBlock2(nn.Module):
-    def __init__(self, nb_channels: int) -> None:
+    def __init__(self, nb_channels: int, init_zero: bool = False) -> None:
         super().__init__()
         self.norm1 = nn.BatchNorm2d(nb_channels, affine=False)
         self.conv1 = nn.Conv2d(
@@ -159,6 +167,8 @@ class ResidualBlock2(nn.Module):
         self.conv2 = nn.Conv2d(
             nb_channels, nb_channels, kernel_size=3, stride=1, padding=1
         )
+        if init_zero:
+            torch.nn.init.zeros_(self.conv2.weight)
 
     def forward(self, x: torch.Tensor, noise_emb: torch.Tensor) -> torch.Tensor:
         nb_channels = self.norm1.num_features
@@ -173,3 +183,90 @@ class ResidualBlock2(nn.Module):
             )
         )
         return x + y
+
+
+class NoiseEmbedding2(nn.Module):
+    def __init__(self, image_size: int) -> None:
+        super().__init__()
+        assert image_size % 2 == 0
+        self.register_buffer("weight", torch.randn(1, image_size**2 // 2))
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        assert input.ndim == 1
+        f = 2 * torch.pi * input.unsqueeze(1) @ self.weight
+        return torch.cat([f.cos(), f.sin()], dim=-1)
+
+
+class Unet1(nn.Module):
+    def __init__(self, image_size: int, channels: int) -> None:
+        super().__init__()
+        self.image_size = image_size
+        self.channels = channels
+        self.noise_emb = NoiseEmbedding2(image_size)
+        self.noise_fc = nn.Linear(image_size**2, image_size**2)
+        self.norm1 = nn.BatchNorm2d(2)
+        self.conv1 = nn.Conv2d(2, channels, kernel_size=3, stride=2, padding=1)
+        self.norm2 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(
+            channels, channels * 2, kernel_size=3, stride=2, padding=1
+        )
+        self.norm3 = nn.BatchNorm2d(channels * 2)
+        self.conv3 = nn.Conv2d(
+            channels * 2, channels * 4, kernel_size=3, stride=2, padding=1
+        )
+        self.norm4 = nn.BatchNorm2d(channels * 4)
+        self.fc1 = nn.Linear(
+            channels * 4 * (image_size // 8) ** 2, channels * 4 * (image_size // 8) ** 2
+        )
+        self.inorm1 = nn.BatchNorm2d(channels * 4)
+        self.iconv1 = nn.ConvTranspose2d(
+            channels * 4, channels * 2, kernel_size=2, stride=2
+        )
+        self.inorm2 = nn.BatchNorm2d(channels * 2)
+        self.iconv2 = nn.ConvTranspose2d(
+            channels * 2, channels, kernel_size=2, stride=2
+        )
+        self.inorm3 = nn.BatchNorm2d(channels)
+        self.iconv3 = nn.ConvTranspose2d(channels, 2, kernel_size=2, stride=2)
+        self.inorm4 = nn.BatchNorm2d(2)
+        self.conv_out = nn.Conv2d(2, 1, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x, cond):
+        cond = self.noise_fc(self.noise_emb(cond))
+        x0 = torch.concat(
+            [
+                x,
+                cond.reshape((-1, 1, self.image_size, self.image_size))
+                * torch.ones(*x.shape, device=x.device),
+            ],
+            axis=1,
+        )
+        x1 = self.conv1(F.relu(self.norm1(x0)))
+        x2 = self.conv2(F.relu(self.norm2(x1)))
+        x3 = self.conv3(F.relu(self.norm3(x2)))
+        x4 = F.relu(
+            self.fc1(
+                F.relu(self.norm4(x3)).reshape(
+                    (-1, self.channels * 4 * (self.image_size // 8) ** 2)
+                )
+            )
+        )
+        x4 = self.iconv1(
+            F.relu(
+                self.inorm1(
+                    x4.reshape(
+                        (
+                            -1,
+                            self.channels * 4,
+                            self.image_size // 8,
+                            self.image_size // 8,
+                        )
+                    )
+                )
+            )
+            + x3
+        )
+        x4 = self.iconv2(F.relu(self.inorm2(x4)) + x2)
+        x4 = self.iconv3(F.relu(self.inorm3(x4)) + x1)
+        x4 = self.conv_out(F.relu(self.inorm4(x4)))
+        return x4
